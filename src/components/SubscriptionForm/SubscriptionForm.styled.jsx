284812d3d38c6bccd71a58a048690ec186de6780
Diana Forostiana <dianaforost@gmail.com>
2023-11-02 17:22:31 +0100

    fix: added padding

--- src/components/SubscriptionForm/SubscriptionForm.styled.jsx
+++ src/components/SubscriptionForm/SubscriptionForm.styled.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const SubscriptionFormSection = styled.section`
   width: 100%;
+  padding: 30px 0;
+  @media (min-width: 1440px) {
+    padding: 60px 0;
+  }
 `;
 
 export const SubscriptionFormWrapper = styled.div`
@@ -9,11 +13,8 @@ export const SubscriptionFormWrapper = styled.div`
   justify-content: space-evenly;
   align-items: center;
   margin: 0 auto;
-  @media screen and (min-width: 360px) {
-    max-width: 360px;
-    padding-left: 28px;
-    padding-right: 28px;
-  }
+  padding-left: 28px;
+  padding-right: 28px;
   @media (min-width: 1440px) {
     max-width: 1360px;
     padding-left: 40px;
@@ -21,34 +22,32 @@ export const SubscriptionFormWrapper = styled.div`
   }
 `;
 export const SubscriptionFormImage = styled.div`
-  width: 418px;
-  height: 400px;
+  width: 330px;
+  height: 315px;
   background-color: #ececec;
-  @media screen and (min-width: 360px) {
-    display: none;
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: block;
   }
   @media (min-width: 1440px) {
-    display: block;
+    width: 418px;
+    height: 400px;
   }
 `;
 export const SubscriptionFormContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  @media screen and (min-width: 360px) {
-    gap: 33px;
-  }
+  gap: 33px;
   @media (min-width: 1440px) {
     gap: 43px;
   }
 `;
 export const SubscriptionFormTitle = styled.h3`
-  @media screen and (min-width: 360px) {
-    width: 192px;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 22px;
-  }
+  width: 192px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 22px;
   @media (min-width: 1440px) {
     width: 459px;
     font-size: 36px;
@@ -60,22 +59,18 @@ export const Form = styled.form`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  @media screen and (min-width: 360px) {
-    width: 304px;
-  }
+  width: 304px;
   @media (min-width: 1440px) {
     width: 534px;
   }
 `;
 export const SubscriptionFormInput = styled.input`
-  @media screen and (min-width: 360px) {
-    width: 100%;
-    padding: 10px 16px 10px 16px;
-    border: 1px solid #000000;
-    font-size: 12px;
-    line-height: 17px;
-    color: #696969;
-  }
+  width: 100%;
+  padding: 10px 16px 10px 16px;
+  border: 1px solid #000000;
+  font-size: 12px;
+  line-height: 17px;
+  color: #696969;
   @media (min-width: 1440px) {
     padding: 14px 16px 14px 16px;
     font-size: 20px;
@@ -84,13 +79,11 @@ export const SubscriptionFormInput = styled.input`
 `;
 export const SubscriptionFormButton = styled.button`
   border: none;
-  @media screen and (min-width: 360px) {
-    padding: 11px 27px;
-    background: #191919;
-    font-size: 12px;
-    line-height: 17px;
-    color: #fff;
-  }
+  padding: 11px 27px;
+  background: #191919;
+  font-size: 12px;
+  line-height: 17px;
+  color: #fff;
   @media (min-width: 1440px) {
     padding: 15px;
     font-size: 20px;
