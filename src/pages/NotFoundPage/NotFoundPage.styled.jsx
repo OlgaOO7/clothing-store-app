84c670ad43aadbf46ced1050bff20d4f1a70262c
Olga007 <o.piankova@ukr.net>
2023-11-11 12:10:19 +0200

    feat: Update not found page
    Update not found page and add styles

---
+++ src/pages/NotFoundPage/NotFoundPage.styled.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  padding-top: 100px;
+`;
+
+export const ContentWrrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  margin-bottom: 120px;
+  text-align: center;
+`;
+
+export const ErrorCode = styled.p`
+  font-weight: 900px;
+  font-size: 200px;
+  color: #000;
+  letter-spacing: 3px;
+  color: #969696;
+`;
+
+export const ErrorText = styled.p`
+  color: #000000;
+  font-size: 18px;
+`;
