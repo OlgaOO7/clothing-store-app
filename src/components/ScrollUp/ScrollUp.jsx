e1066907f054cd15d152f265888214a8fc4ee021
Diana Forostiana <dianaforost@gmail.com>
2023-11-26 18:18:37 +0100

    feat: scroll-up

---
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -0,0 +1,5 @@
+export const ScrollUp = () => {
+  return (
+    <div style={{ width: '10px', height: '10px', background: 'gray' }}></div>
+  );
+};
--- src/components/SharedLayouts/SharedLayouts.jsx
+++ src/components/SharedLayouts/SharedLayouts.jsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from '../Header/Header';
 import { Footer } from 'components/Footer/Footer';
+import { ScrollUp } from 'components/ScrollUp/ScrollUp';
 
 export const SharedLayout = () => {
   return (
     <div>
       <Header />
+      <ScrollUp />
       <Outlet />
       <Footer />
     </div>
