a740507642cb6b6d04667db5a530a59e4df9912c
Diana Forostiana <dianaforost@gmail.com>
2024-01-03 15:50:33 +0100

    fix: contact form

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -21,7 +21,7 @@ export const InputField = ({
             placeholder={placeholder}
             onKeyUp={handleInputChange}
             maxLength={maxLength}
-            $errors={errors}
+            $errors={errors[name]}
           ></Textarea>
           <Counter style={{ color: isApproachingMax ? 'red' : '' }}>
             {charCount}/{maxLength}
@@ -36,7 +36,7 @@ export const InputField = ({
             type={type}
             placeholder={placeholder}
             autoComplete="true"
-            $errors={errors}
+            $errors={errors[name]}
           />
           {errors[name] && <ErrorMessage>{errors[name].message}</ErrorMessage>}
         </div>
