cffdd83400eb9135c8ffed70a516f2e54f9a7568
Olga007 <o.piankova@ukr.net>
2024-01-23 12:44:24 +0200

    fix mobile searchbar
    Add useMedia hook for mobile version.
    Add useEffect for SearchBarMob closing  when navigate between pages.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,36 @@
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
 import { Navigation } from 'components/Navigation/Navigation';
 import { SearchBarMob } from 'components/SearchBar/SearchBarMob';
+import { useMedia } from 'hooks/useMedia';
 
 import { HeaderWrapper, NavigationContainer } from './Header.styled';
-import React from 'react';
 
 export const Header = () => {
   const [isShownSearching, setIsShownSearching] = useState(false);
+  const { isMobileScreen } = useMedia();
+  const location = useLocation();
 
   const toggleShowSearch = () => {
     setIsShownSearching(!isShownSearching);
   };
 
+  useEffect(() => {
+    setIsShownSearching(false);
+  }, [location.pathname]);
+
   return (
     <HeaderWrapper>
       <NavigationContainer>
         <Navigation sectionType="header" toggleShowSearch={toggleShowSearch} />
-        <SearchBarMob
-          isShowSearching={isShownSearching}
-          toggleShowSearch={toggleShowSearch}
-          sectionType="header"
-        />
+        {isMobileScreen && (
+          <SearchBarMob
+            isShowSearching={isShownSearching}
+            toggleShowSearch={toggleShowSearch}
+            sectionType="header"
+          />
+        )}
       </NavigationContainer>
     </HeaderWrapper>
   );
