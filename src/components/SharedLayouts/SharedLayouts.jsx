2aae00e8267408f81c065c7fdceb2091fd3e31bc
Olga007 <o.piankova@ukr.net>
2024-02-08 22:34:56 +0200

    fix: add main, set flexible length

--- src/components/SharedLayouts/SharedLayouts.jsx
+++ src/components/SharedLayouts/SharedLayouts.jsx
@@ -6,21 +6,26 @@ import { Footer } from 'components/Footer/Footer';
 import { ScrollUp } from 'components/ScrollUp/ScrollUp';
 import { Loader } from 'components/Loader/Loader';
 
+import { Container, Main } from './SharedLayouts.styled';
+
 export const SharedLayout = () => {
   return (
-    <div>
+    <Container>
       <Header />
       <ScrollUp />
-      <Suspense
-        fallback={
-          <div style={{ paddingTop: '200px', paddingBottom: '200px' }}>
-            <Loader />
-          </div>
-        }
-      >
-        <Outlet />
-      </Suspense>
+      <Main>
+        <Suspense
+          fallback={
+            <div style={{ paddingTop: '200px', paddingBottom: '200px' }}>
+              <Loader />
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
+      </Main>
+
       <Footer />
-    </div>
+    </Container>
   );
 };
